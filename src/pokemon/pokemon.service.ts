2f338de30d971b39421e6db110ae9b0d41aa0fde
Jesus Eduardo Facio <lalo.codein22@gmail.com>
2024-01-17 13:37:43 -0600

    Add CommonModule and update remove method in PokemonController

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -72,8 +72,14 @@ export class PokemonService {
 
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} pokemon`;
+	async remove(id: string) {
+		const result = await this.pokemonModel.findByIdAndDelete(id);
+ 
+    if (!result) {
+      throw new NotFoundException(`Pokemon with id: "${id}" not found`);
+    }
+ 
+    return result;
 	}
 	
 	private handleExceptions(error: any) {
